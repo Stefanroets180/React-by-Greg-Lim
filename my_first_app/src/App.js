93ebc298a61105f806a814d132fced68a5f5140f
Stefan <61413955+Stefanroets180@users.noreply.github.com>
2021-03-03 12:39:55 +0200

    Chapter 7 GETTING DATA FROM RESTFUL APIS WITH AXIOS

--- my_first_app/src/App.js
+++ my_first_app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import GitHub from './GitHub';
 //import JumboTronComponent from './JumboTronComponent';
 //import Products from './Products';
-import UserForm from './UserForm';
+//import UserForm from './UserForm';
 
 class App extends Component {
   render() {        
     return (
       <div>
-        <UserForm/>        
+        <GitHub/>        
       </div>
     );
   }
